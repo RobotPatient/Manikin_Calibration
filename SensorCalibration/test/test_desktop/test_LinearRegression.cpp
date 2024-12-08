61888f4e501b54d5deb3e0ff8eeda75d7edf7190
Thomas van den Oever <thomas.vanden.oever@capgemini.com>
2024-12-08 21:26:17 +0100

    -fix floating point check now use doctest::Approx()

--- SensorCalibration/test/test_desktop/test_LinearRegression.cpp
+++ SensorCalibration/test/test_desktop/test_LinearRegression.cpp
@@ -1,22 +1,22 @@
-#define DOCTEST_CONFIG_IMPLEMENT  // REQUIRED: Enable custom main()
+#define DOCTEST_CONFIG_IMPLEMENT // REQUIRED: Enable custom main()
 #include <doctest.h>
 
 #include "LinearRegression.hpp"
 
 TEST_CASE("Testing linear regression functions")
 {
-    static const int n = 5;
-    float x[n] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f};//, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f};
-    float y[n] = {2.0f, 4.0f, 5.0f, 4.0f, 5.0f};//, 6.02f, 6.5f, 8.1f, 8.2f, 9.9f};
+  static const int n = 5;
+  float x[n] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f}; //, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f};
+  float y[n] = {2.0f, 4.0f, 5.0f, 4.0f, 5.0f}; //, 6.02f, 6.5f, 8.1f, 8.2f, 9.9f};
 
-    struct linearValue myValue;
+  struct linearValue myValue;
 
-    LinearRegression test(y, x, n);
-    myValue = test.calcAlphaBeta();
-    CHECK(myValue.alpha == 0.6f);
-    CHECK(myValue.beta == 2.2f);
-    CHECK(test.calcR2() == 0.6f);
-    CHECK(test.calcStandardError() == 0.89f);
+  LinearRegression test(y, x, n);
+  myValue = test.calcAlphaBeta();
+  REQUIRE(myValue.alpha == 0.6f);
+  REQUIRE(myValue.beta == doctest::Approx(2.2f));
+  CHECK(test.calcR2() == doctest::Approx(0.6f));
+  CHECK(test.calcStandardError() == doctest::Approx(0.89f).epsilon(0.01));
 }
 // TEST_SUITE ...
 
