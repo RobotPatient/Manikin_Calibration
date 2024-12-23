71a41df17948cbd24ddaf653ff70d1100e3bf992
Thomas van den Oever <thomas.vanden.oever@capgemini.com>
2024-12-23 13:31:27 +0100

    -fix fixed some bugs offset, calibrationfactor and samplesize now get set correctly Measurement taking switched form polling to read on a serial signal

--- SensorCalibration/src/arduino/main.cpp
+++ SensorCalibration/src/arduino/main.cpp
@@ -7,6 +7,7 @@
 #include "SensorComs.hpp"
 #include "Sensorhub_definitions.hpp"
 #include "Serial.hpp"
+#include "ads7138_definitions.hpp"
 #include "ads7138.hpp"
 #include "nau7802.hpp"
 
@@ -14,36 +15,34 @@ ads7138 *fingerPositionSensor;
 nau7802 *loadCell;
 
 unsigned int i = 0;
-unsigned int n = 50;
-float referenceValues[50], vingerposition[50];
-int sOffset = 0x0000;
+unsigned int sampleSize;
+float referenceValues[MAX_SAMPLE], vingerposition[ADS_CHANNEL_AMOUNT][MAX_SAMPLE];
+int32_t sOffset = 0x0000;
 float sCalFactor = 0.0f;
 char receivedBytes[255];
 char receivedBytes2[255];
 
-void pain()
+void readValues(int32_t &intValue, float &floatValue, unsigned int &value)
 {
-  while (Serial.available() == 0)
-  { // TODO: timeout?
-  }
-  for (int i = 0; i < Serial.available(); i++)
-  {
-    if (receivedBytes[i] == '|')
-    {
-      receivedBytes2[i] = (char)Serial.read();
-    }
-    else
-    {
-      receivedBytes[i] = (char)Serial.read();
-    }
-  }
+  String input;
 
-  sCalFactor = atof(receivedBytes);
-  sOffset = atoi(receivedBytes2);
+  // Lees de tweede float waarde tot aan de newline
+  input = Serial.readStringUntil(' ');
+  floatValue = input.toFloat();
 
-  Serial.println(sCalFactor);
-  Serial.println(sOffset);
-  loadCell->setCalibrationValues((uint32_t)sOffset, sCalFactor, 0);
+  // Lees de eerste int32_t waarde tot aan de spatie
+  input = Serial.readStringUntil(' ');
+  intValue = input.toInt();
+
+  // Lees de eerste int32_t waarde tot aan de spatie
+  input = Serial.readStringUntil('\n');
+  value = input.toInt();
+
+  Serial.print(floatValue);
+  Serial.print(" ");
+  Serial.print(intValue);
+  Serial.print(" ");
+  Serial.println(value);
 }
 
 void setup()
@@ -56,10 +55,12 @@ void setup()
   fingerPositionSensor->resetStatus();
   SerialPC::waitForSerial();
 
-  // pain();
-
+  readValues(sOffset, sCalFactor, sampleSize);
+  loadCell->setCalibrationValues(sOffset, sCalFactor, 0);
+  loadCell->calibrateInternal();
   // loadCell->calibrateExternal();
-  loadCell->setCalibrationValues(136054, 436.32, 0);
+
+  // loadCell->setCalibrationValues(136054, 436.32, 0);
 
   SerialPC::printColum();
 }
@@ -68,28 +69,40 @@ void loop()
 {
   heartbeat::updateHeartBeat();
 
-  if (heartbeat::runPolling())
+  // if (heartbeat::runPolling())
+  if (Serial.available() > 0) // Only measure when character is send
   {
+    Serial.read(); // consume character
+
     loadCell->readLoadCell();
     fingerPositionSensor->readValues();
     // TODO: write to memory? (sd card needed on hardware stuffs)
     if (Serial)
     {
       referenceValues[i] = loadCell->getValue();
-      vingerposition[i] = fingerPositionSensor->getValue(0);
+      for (int channel = 0; channel < 8; channel++)
+      {
+        vingerposition[channel][i] = fingerPositionSensor->getValue(channel);
+      }
       Serial.print(i++);
       loadCell->printValue(SEPERATION_CHAR);
       fingerPositionSensor->printValues(SEPERATION_CHAR);
       Serial.println();
+      Serial.flush();
     }
 
-    if (i == n) // if there are n samples, calculate the regression values
+    if (i == sampleSize) // if there are n samples, calculate the regression values
     {
-      LinearRegression fingerPositionRegression = LinearRegression(referenceValues, vingerposition, n);
-      struct linearValue myValue = fingerPositionRegression.calcAlphaBeta();
-      float r2 = fingerPositionRegression.calcR2();
-      float se = fingerPositionRegression.calcStandardError();
-      SerialPC::printRegression(myValue, r2, se);
+      for (int channel = 0; channel < 8; channel++)
+      {
+        LinearRegression fingerPositionRegression = LinearRegression(referenceValues, vingerposition[channel], sampleSize);
+        struct linearValue myValue = fingerPositionRegression.calcAlphaBeta();
+        float r2 = fingerPositionRegression.calcR2();
+        float se = fingerPositionRegression.calcStandardError();
+        SerialPC::printRegression(myValue, r2, se, channel);
+      }
+      Serial.println();
+      Serial.flush();
     }
   }
 }
